23e9a172aad629bce1905567d505dc45fe647784
Bastien LAFOSSE <bastien_du_21@hotmail.fr>
2024-04-22 23:57:13 +0200

    Added files to gitignore

--- src/bkp_env.sh
+++ src/bkp_env.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# DIST FILE / Update values before launching script
+
 # DNS Records that you wish to be modified if needed
-DNSDomain="lafosse.tech" # Domain under which your records can be found
-DNSRecordV4="roamingv4"  # A Type Record
-DNSRecordV6="roamingv6"  # AAAA Type Record
+DNSDomain="test"    # Domain under which your records can be found
+DNSRecordV4=""  # A Type Record
+DNSRecordV6=""  # AAAA Type Record
 
 # Provider - Gandi
-Gandi_APIToken="062331537b83cae5de284cd563512f153b721512"
+Gandi_APIToken=""
