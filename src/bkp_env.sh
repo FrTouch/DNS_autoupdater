48dba349ea787196fb31610df98136e126918e4d
Bastien LAFOSSE <bastien_du_21@hotmail.fr>
2024-11-14 21:06:54 +0100

    None

--- src/bkp_env.sh
+++ src/bkp_env.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-# DIST FILE / Update values before launching script
+# Global Parameters
+DNSProvider="Gandi"
 
 # DNS Records that you wish to be modified if needed
-DNSDomain="test"    # Domain under which your records can be found
-DNSRecordV4=""  # A Type Record
-DNSRecordV6=""  # AAAA Type Record
+DNSDomain="lafosse.tech" # Domain under which your records can be found
+DNSRecordV4="roamingv4"  # A Type Record
+DNSRecordV6="roamingv6"  # AAAA Type Record
 
 # Provider - Gandi
-Gandi_APIToken=""
+GandiAPIToken="062331537b83cae5de284cd563512f153b721512"
+GandiRecordTTL="600"
+
+# Provider - OVH
